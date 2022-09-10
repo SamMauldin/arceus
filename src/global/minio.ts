5f3d0b4512bf540db56f35fb1061fd2349ca7f68
Sam Mauldin <sam@mauldin.me>
2022-09-10 02:48:50 +0000

    minio config change

--- src/global/minio.ts
+++ src/global/minio.ts
@@ -2,8 +2,8 @@ import { Client } from 'minio';
 
 export const minio = new Client({
   endPoint: process.env.MINIO_ENDPOINT!,
-  port: 443,
-  useSSL: true,
+  port: 9000,
+  useSSL: false,
   accessKey: process.env.MINIO_KEY_ID!,
   secretKey: process.env.MINIO_KEY_SECRET!,
 });
